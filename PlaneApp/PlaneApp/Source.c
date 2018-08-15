0dc87e6e55294a144f18ff54227f865f0c2cd8b7
Aron Gaspic <arongaspic1@gmail.com>
2018-08-15 14:04:05 +0200

    Update PlaneApp
    update

--- PlaneApp/PlaneApp/Source.c
+++ PlaneApp/PlaneApp/Source.c
@@ -1,11 +1,13 @@
 // aplikacija za usmeni ispit iz kolegija Programiranje II. Aplikacija rezerviranja mjesta u avionu. Aron Gaspic
+// exam application for Programming II subject on college. Plane seat booking/checking app. Aron Gaspic 
 #include <stdio.h>
 #include "Methods.h" // my header file
 
 int main()
 {
 	printf("Dobrodosli u aplikaciju za rezervaciju avionskih mjesta.\n");
-	flightName = (char*)malloc((FLIGHTNAMELONG) * sizeof(char)); // alocate memory for flight name
+	//printf("Welcome to application. \n");
+	flightName = (char*)malloc((FLIGHTNAMELONG) * sizeof(char)); // allocate memory for flight name
 
 	setFlightName();
 
